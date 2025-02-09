1ab2ae3df474fdc9044695d0a1f458240092b12f
mustafoyev202 <161561397+mustafoyev202@users.noreply.github.com>
2025-02-09 21:12:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import asyncio
 import logging
 import sys
 import wikipedia
-
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
+from dotenv import load_dotenv
+import os
 
-TOKEN = "6782246368:AAFcPnOqfTHQSZO9QTosdcEAcNxL_mNYAbc"
-wikipedia.set_lang('uz')
+load_dotenv()  # Load environment variables from .env file
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Get the token from environment variable
+wikipedia.set_lang("uz")
 dp = Dispatcher()
+
+PORT = 8080  # Specify the port
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
     await message.reply(
-        f"Hello, {hbold(message.from_user.full_name)}! I will help you to find out more info about any topic")
+        f"Hello, {hbold(message.from_user.full_name)}! I will help you to find out more info about any topic"
+    )
 
 
 @dp.message()
@@ -31,7 +37,7 @@ async def echo_handler(message: types.Message):
 
 async def main():
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, port=PORT)  # Use the specified port
 
 
 if __name__ == "__main__":
